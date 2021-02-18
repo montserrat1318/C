81f513a283000f0caeb40c3a9764121317125034
montserrat1318 <78569514+montserrat1318@users.noreply.github.com>
2021-02-17 18:51:35 -0600

    Add files via upload
    im learning with youtube too:´) to improve myself<3

---
+++ estoy aprendiendo en youtu parte 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+    int puntos=40;
+    float tiempo=10.45;
+    printf("\tPuntos: %09i\n\n\tTiemp:%.3f\n",puntos,tiempo);
+    return 0;
+}
---
+++ estoy aprendiendo en youtu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int puntos= 100;
+    char sexo= 'F';
+    float tiempo= 10.89;
+    double distancia=1020.56;
+
+    printf ("Puntos: %i\n Sexo: %c\n Tiempo: %f\n Dist: %lf\n", puntos, sexo,tiempo, distancia);
+    return 0;
+}
