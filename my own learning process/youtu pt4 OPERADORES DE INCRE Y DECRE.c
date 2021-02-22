f880a1436a5094d310a6b5a80911ecc37645d59b
montserrat1318 <78569514+montserrat1318@users.noreply.github.com>
2021-02-22 12:40:29 -0600

    my own learning process:)
    in here i really try to be more cool and a little bit better in progra, youtube and vids must help, right? lol

---
+++ my own learning process/youtu pt4 OPERADORES DE INCRE Y DECRE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int nivel=1;
+    int vidas=3;
+    //nivel=nivel+1;
+    //nivel++;
+    //vidas=vidas-1;
+    //vidas--;
+    printf("Vidas: %d\n",vidas-1);
+    printf("Nivel: %d\n",nivel++);
+
+
+    return 0;
+}
